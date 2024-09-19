7da49e5b16683df2149682b49aca369817caa69a
Chris Caskey <chris.caskey@outlook.com>
2024-09-19 17:50:07 +0100

    Add click handler to list group

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,22 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
+  let items = ["Hull", "Lincoln", "Bedford", "London"];
+
+  const handleClick = (event: MouseEvent) => console.log(event);
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">List Group Item</li>
-      <li className="list-group-item">List Group Item</li>
-      <li className="list-group-item">List Group Item</li>
-      <li className="list-group-item">List Group Item</li>
-    </ul>
+    <>
+      <h1>Places Lived</h1>
+      <ul className="list-group">
+        {items.length === 0 && <li className="list-group-item">No items</li>}
+        {items.map((item) => (
+          <li key={item} className="list-group-item" onClick={handleClick}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
