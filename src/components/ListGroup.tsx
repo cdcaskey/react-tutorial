00c99924bbed77c73bf360bba08dc09ccce3231f
Chris Caskey <chris.caskey@outlook.com>
2024-09-19 01:02:35 +0100

    Add list group component

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,12 @@
+function ListGroup() {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">List Group Item</li>
+      <li className="list-group-item">List Group Item</li>
+      <li className="list-group-item">List Group Item</li>
+      <li className="list-group-item">List Group Item</li>
+    </ul>
+  );
+}
+
+export default ListGroup;
